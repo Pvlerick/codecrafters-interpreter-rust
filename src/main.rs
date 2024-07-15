d7eac706ef7421b3aa8f5d24152e7d416bf5bdb5
Philippe Vlérick <pvlerick@gmail.com>
2024-07-15 10:51:05 +0200

    Scanning: Parentheses

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::env;
+use std::fmt::Display;
 use std::fs;
 use std::io::{self, Write};
+use std::usize;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -22,16 +24,88 @@ fn main() {
                 String::new()
             });
 
-            // Uncomment this block to pass the first stage
             if !file_contents.is_empty() {
-                panic!("Scanner not implemented");
+                for token in tokenize(file_contents) {
+                    println!("{}", token);
+                }
+                println!("EOF  null");
             } else {
-                println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
+                println!("EOF  null");
             }
         }
         _ => {
             writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
             return;
+        }
+    }
+}
+
+fn tokenize(content: String) -> Vec<Token> {
+    let mut tokens = Vec::new();
+
+    for (line_number, line_content) in content.lines().enumerate() {
+        let chars = line_content.chars().collect::<Vec<_>>();
+        for (char_position, c) in chars.iter().enumerate() {
+            let lexeme = chars[char_position..char_position + c.len_utf8()]
+                .iter()
+                .map(|&i| i as u8)
+                .collect::<Vec<_>>();
+            match c {
+                '(' => tokens.push(Token::left_par(lexeme, line_number)),
+                ')' => tokens.push(Token::right_par(lexeme, line_number)),
+                _ => {}
+            }
+        }
+    }
+
+    tokens
+}
+
+#[derive(Debug)]
+struct Token {
+    token_type: TokenType,
+    lexeme: String,
+    literal: Option<String>,
+    line: usize,
+}
+
+impl Token {
+    fn left_par(lexeme: Vec<u8>, line: usize) -> Token {
+        Token {
+            token_type: TokenType::LeftParenthesis,
+            lexeme: String::from_utf8(lexeme).unwrap(),
+            literal: None,
+            line,
+        }
+    }
+
+    fn right_par(lexeme: Vec<u8>, line: usize) -> Token {
+        Token {
+            token_type: TokenType::RightParenthesis,
+            lexeme: String::from_utf8(lexeme).unwrap(),
+            literal: None,
+            line,
+        }
+    }
+}
+
+impl Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {} null", self.token_type, self.lexeme)
+    }
+}
+
+#[derive(Debug)]
+enum TokenType {
+    LeftParenthesis,
+    RightParenthesis,
+}
+
+impl Display for TokenType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self {
+            TokenType::LeftParenthesis => write!(f, "LEFT_PAREN"),
+            TokenType::RightParenthesis => write!(f, "RIGHT_PAREN"),
         }
     }
 }
