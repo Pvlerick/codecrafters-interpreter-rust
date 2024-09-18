601f3e6e50c592fc8fe12839a7751d1d2ac2346f
Philippe Vlérick <pvlerick@gmail.com>
2024-09-18 15:43:53 +0200

    Print: Multiple statements

--- tests/interpreter_run.rs
+++ tests/interpreter_run.rs
@@ -25,3 +25,65 @@ fn run_print_true() {
     assert!(res.is_ok());
     assert_eq!("true\n", String::from_utf8_lossy(&output));
 }
+
+#[test]
+fn run_print_multiple_statements() {
+    let mut tmp_file = TempFile::with_content("print \"foo\"; print 42;");
+    let mut interpreter = Interpreter::build(tmp_file.reader()).unwrap();
+    let mut output = Vec::new();
+    let res = interpreter.run(&mut output, &mut sink());
+    assert!(res.is_ok());
+    assert_eq!(
+        r#"foo
+42
+"#,
+        String::from_utf8_lossy(&output)
+    );
+}
+
+#[test]
+fn run_print_multiple_lines_1() {
+    let mut tmp_file = TempFile::with_content(
+        r#"print "foo";
+print 42;"#,
+    );
+    let mut interpreter = Interpreter::build(tmp_file.reader()).unwrap();
+    let mut output = Vec::new();
+    let res = interpreter.run(&mut output, &mut sink());
+    assert!(res.is_ok());
+    assert_eq!(
+        r#"foo
+42
+"#,
+        String::from_utf8_lossy(&output)
+    );
+}
+
+#[test]
+fn run_print_multiple_lines_2() {
+    let mut tmp_file = TempFile::with_content(
+        r#"
+print true != true;
+
+print "36
+10
+78
+";
+
+print "There should be an empty line above this.";"#,
+    );
+    let mut interpreter = Interpreter::build(tmp_file.reader()).unwrap();
+    let mut output = Vec::new();
+    let res = interpreter.run(&mut output, &mut sink());
+    assert!(res.is_ok());
+    assert_eq!(
+        r#"false
+36
+10
+78
+
+There should be an empty line above this.
+"#,
+        String::from_utf8_lossy(&output)
+    );
+}
